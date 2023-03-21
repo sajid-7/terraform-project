0bb44a5cf55a3c38627e90fed21d4ca66ec37b35
Shaikh Sajid <121095123+sajid-7@users.noreply.github.com>
2023-03-21 23:43:53 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
   name               = "raham-terraform-elb"
-  availability_zones = ["ap-northeast-1c", "ap-northeast-1b"]
+  availability_zones = ["ap-northeast-1a", "ap-northeast-1c"]
 
   listener {
     instance_port     = 80
