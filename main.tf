972a6619b22b6217356571f43042fff843ebcd88
Shaikh Sajid <121095123+sajid-7@users.noreply.github.com>
2023-03-21 23:39:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "aws_security_group" "three" {
 }
 
 resource "aws_s3_bucket" "four" {
-  bucket = "raham0077552bucketterra"
+  bucket = "myapnortheastb"
 }
 
 resource "aws_iam_user" "five" {
